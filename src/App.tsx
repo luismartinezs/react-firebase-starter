837df67ed210376552bf5faa2f811bfd3d32a01b
Luis Martinez <luis@passionatepeople.io>
2022-07-14 14:03:25 +0200

    chore: remove custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,6 @@ function App() {
           <BrowserRouter>
             <MantineProvider
               theme={{
-                // Override any other properties from default theme
-                fontFamily: 'Open Sans, sans serif',
-                spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
                 colorScheme: 'dark',
               }}
             >
